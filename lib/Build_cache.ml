a8831eda6ab9b8d13ccb419d9488f7609b09aee8
Kento Okura <kentookura@gmail.com>
2024-07-24 21:48:30 +0200

    change content type of irmin store

--- lib/Build_cache.ml
+++ lib/Build_cache.ml
@@ -23,53 +23,58 @@ and process_dir dir =
 let scan_directories dirs =
   S.run @@ fun () -> dirs |> List.iter @@ fun fp -> process_dir fp
 
-module Content : Irmin.Contents.S with type t = string = struct
+module Content : Irmin.Contents.S with type t = Forester_core.Code.t = struct
   open Irmin.Type
 
-  type t = string
+  type t = Forester_core.Code.t
 
-  let t = string
+  let t = Forester_irmin.Code.t
   let merge ~old:_ _a b = Irmin.Merge.ok b
   let merge = Irmin.Merge.(option (v t merge))
 end
 
-module Store = Irmin_fs_unix.KV.Make (Content)
+module Cache = Irmin_fs_unix.KV.Make (Content)
 
-type foo = { contents : string; last_changed : float; path : string option }
+(* NOTE:
+   Not every irmin store supports the Sync API. This means we can only use the
+   store module defined in this file for the build cache. For remote forestry,
+   we should fetch the compiled format.
+*)
 
-let _read_trees_in_dirs ~dev ?(_ignore_malformed = false) dirs build_cache =
+type foo = {
+  contents : Forester_core.Code.t;
+  last_changed : float;
+  path : string option;
+  addr : string;
+}
+
+let read_trees_in_dirs ~dev ?(_ignore_malformed = false) dirs
+    (build_cache : Cache.t) =
   scan_directories dirs |> List.of_seq
   |> ( List.filter_map @@ fun fp ->
        match Eio.Path.split fp with
-       | Some (_, basename) ->
+       | Some (_, basename) -> (
            let addr = Filename.chop_extension basename in
            let source_path =
              if dev then Option.map Unix.realpath @@ Eio.Path.native fp
              else None
            in
            let mtime = Eio.Path.(stat ~follow:false fp).mtime in
-           Some
-             {
-               contents = Eio.Path.load fp;
-               last_changed = mtime;
-               path = source_path;
-             }
+           match Forester_frontend.Parse.parse_file fp with
+           | Ok code ->
+               Some
+                 {
+                   contents = code;
+                   last_changed = mtime;
+                   path = source_path;
+                   addr;
+                 }
+           | Error _ -> None)
        | None -> None )
-  |> Lwt_list.iter_p (fun { contents; last_changed; path } ->
-         let _ = Store.Backend.Contents.Hash.hash contents in
-         let info () = Store.Info.v 0L in
-         Store.set_exn build_cache [ "sources"; addr ] ~info tree)
+  |> Lwt_list.iter_p (fun { contents; last_changed; path; addr } ->
+         let hash = Cache.Backend.Contents.Hash.hash contents in
+         let key = Cache.Contents.of_hash (Cache.repo build_cache) hash in
+         let info () = Cache.Info.v 0L in
+         Cache.set_exn build_cache [ "sources"; addr ] ~info contents)
 
 let _make_dir ~env dir = Eio.Path.(Eio.Stdenv.fs env / dir)
-
-let main _env =
-  let config = Irmin_fs.config "build/cache" in
-  let* repo = Store.Repo.v config in
-  let* main = Store.main repo in
-  let info () = Store.Info.v 0L in
-  let key = "Hello" in
-  let* () = Store.set_exn main [ key ] ~info "world!" in
-  let* v = Store.get main [ key ] in
-  print_endline v |> Lwt.return
-
-let () = Eio_main.run @@ fun env -> Lwt_main.run @@ main env
