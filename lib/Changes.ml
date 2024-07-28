2cefd046d4a3002e243d4ab5a345ca9d1ba6bd45
Kento Okura <kentookura@gmail.com>
2024-07-28 08:44:26 +0000

    update for changes in 4.2

--- lib/Changes.ml
+++ lib/Changes.ml
@@ -11,14 +11,18 @@ module S = Set.Make (struct
 end)
 
 (* The set of relations in a query. We might want something more fine grained *)
-let rec relations (q : 'a Query.t) : S.t =
-  match view q with
+let rec relations (q : Sem.query) : S.t =
+  match q with
   | Rel ((_, _, rel), addr) -> S.singleton rel
   | Isect qs | Union qs ->
       List.fold_left S.union S.empty (List.map relations qs)
   | Complement q -> relations q
-  | Isect_fam (query, (_, _, rel)) | Union_fam (query, (_, _, rel)) ->
+  | Isect_fam (query, QClo_rel (_, _, rel))
+  | Union_fam (query, QClo_rel (_, _, rel)) ->
       S.union (S.singleton rel) (relations query)
+  (* TODO: I put S.empty to compile, I have to come back to this*)
+  | Isect_fam (query, Sem.QClo (_, _, _)) -> S.empty
+  | Union_fam (query, Sem.QClo (_, _, _)) -> S.empty
 
 (* One for each builtin relation. Expand on this if you want to create a
    tree-editor.*)
